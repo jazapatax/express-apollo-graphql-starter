b24a2240c8bffd9f8c8238a87fd7e5d48637819b
null-point-sys <jaalza@gmail.com>
2020-04-21 17:26:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express  = require("express");
 const path  = require("path"); // to heroku deploy
 
 // Connect to Atlas remote mongo database:
-   /*const mongoose = require("mongoose");
+     const mongoose = require("mongoose");
      require("dotenv").config({ path: "variables.env" });
      mongoose
      .connect(process.env.MONGO_URI, { useNewUrlParser: true })
      .then(() => console.log("Atlas DB connected"))
-     .catch(err => console.error(err));*/
+     .catch(err => console.error(err));
 // end Connect to Atlas remote mongo database
 
 // models (mongoose schemas):
